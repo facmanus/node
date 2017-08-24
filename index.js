71e5868ee28ceca0c6f4b7d9ed7e5f4f98cbd2ab
facmanus <fac.manus@gmail.com>
2017-08-24 11:07:38 +0900

    'first'

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = express();
+var morgan = require('morgan');
+var users = [
+    {id: 1, name: 'alice'},
+    {id: 2, name: 'bek'},
+    {id: 3, name: 'chris'}
+];
+
+app.use(morgan('dev'));
+
+app.get('/users', function(req, res) {
+    res.json(users);
+});
+
+app.listen(3000, function() {
+    console.log('Exampe app listening on port 3000!');
+
+});
